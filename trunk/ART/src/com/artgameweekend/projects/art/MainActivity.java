027963a6eaadda33e34ae3427d044ab3536ca9d9
pierre@androidsoft.org <pierre@androidsoft.org@c2dd0ccd-b6f9-2fe6-36c0-c3bc145f12f7>
2010-06-04 22:24:16 +0000

    PaintFinger included

--- trunk/ART/src/com/artgameweekend/projects/art/MainActivity.java
+++ trunk/ART/src/com/artgameweekend/projects/art/MainActivity.java
@@ -2,23 +2,47 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.artgameweekend.projects.art;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  *
  * @author pierre
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener
+{
+
+    Button mButtonDraw;
 
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle icicle) {
+    public void onCreate(Bundle icicle)
+    {
         super.onCreate(icicle);
+
+        setContentView(R.layout.main);
+
+        mButtonDraw = (Button) findViewById(R.id.button_draw);
+        mButtonDraw.setOnClickListener(this);
+
         // ToDo add your GUI initialization code here        
     }
 
+    public void onClick(View view)
+    {
+        if (view == mButtonDraw)
+        {
+            Log.d("MainActivity", "onClick:DRAW");
+            Intent intent = new Intent();
+            intent.setClassName("com.artgameweekend.projects.art", "com.artgameweekend.projects.art.FingerPaint");
+            startActivity(intent);
+        }
+    }
 }
