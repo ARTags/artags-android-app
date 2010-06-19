f3d74aa47e03a0a529773d43db68519d3c20a45a
pierre@androidsoft.org <pierre@androidsoft.org@c2dd0ccd-b6f9-2fe6-36c0-c3bc145f12f7>
2010-06-19 10:48:05 +0000

    Separate View from Draw Activity

--- trunk/ART/src/com/artgameweekend/projects/art/R.java
+++ trunk/ART/src/com/artgameweekend/projects/art/R.java
@@ -12,16 +12,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int bt_display=0x7f020000;
-        public static final int bt_draw=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int menu_blur=0x7f020003;
-        public static final int menu_brush=0x7f020004;
-        public static final int menu_color=0x7f020005;
-        public static final int menu_emboss=0x7f020006;
-        public static final int menu_erase=0x7f020007;
-        public static final int menu_save=0x7f020008;
-        public static final int splash=0x7f020009;
-        public static final int splash_land=0x7f02000a;
+        public static final int bt_display_down=0x7f020001;
+        public static final int bt_draw=0x7f020002;
+        public static final int bt_draw_down=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int menu_blur=0x7f020005;
+        public static final int menu_brush=0x7f020006;
+        public static final int menu_color=0x7f020007;
+        public static final int menu_emboss=0x7f020008;
+        public static final int menu_erase=0x7f020009;
+        public static final int menu_save=0x7f02000a;
+        public static final int splash=0x7f02000b;
+        public static final int splash_land=0x7f02000c;
     }
     public static final class id {
         public static final int button_display=0x7f050004;
@@ -47,6 +49,7 @@ public final class R {
         public static final int button_view=0x7f040002;
         public static final int cancel=0x7f04000f;
         public static final int dialog_enter_title=0x7f040010;
+        public static final int dialog_progress=0x7f040012;
         public static final int dialog_send=0x7f040011;
         public static final int menu_blur=0x7f040008;
         public static final int menu_brush_size=0x7f04000a;
@@ -57,5 +60,7 @@ public final class R {
         public static final int menu_srcatop=0x7f04000b;
         public static final int ok=0x7f04000d;
         public static final int send=0x7f04000e;
+        public static final int upload_failed=0x7f040014;
+        public static final int upload_successful=0x7f040013;
     }
 }
