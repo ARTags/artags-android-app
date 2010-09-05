8b770351c48094237206e48471d35b0fd17139e0
pierre@androidsoft.org <pierre@androidsoft.org@c2dd0ccd-b6f9-2fe6-36c0-c3bc145f12f7>
2010-09-05 22:35:25 +0000

    Brush parameters saved between two sessions

--- trunk/ART/src/com/artgameweekend/projects/art/preferences/PreferencesService.java
+++ trunk/ART/src/com/artgameweekend/projects/art/preferences/PreferencesService.java
@@ -18,6 +18,7 @@ package com.artgameweekend.projects.art.preferences;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import com.artgameweekend.projects.art.draw.BrushParameters;
 
 /**
  *
@@ -58,6 +59,40 @@ public class PreferencesService
 
     }
 
+    private static final String KEY_BRUSH_SIZE = "brush.size";
+    private static final String KEY_BRUSH_COLOR = "brush.color";
+    private static final String KEY_BRUSH_COLOR_BASE = "brush.color.base";
+    private static final String KEY_BRUSH_COLOR_INTENSITY = "brush.color.intensity";
+    private static final String KEY_BLUR_EFFECT = "brush.blur";
+    private static final String KEY_EMBOSS_EFFECT = "brush.emboss";
 
+    private static final int DEFAULT_BRUSH_SIZE = 12;
+    private static final int DEFAULT_COLOR = 0xFFA5C739;
+    private static final int DEFAULT_INTENSITY = 50;
+
+    public void saveBrushParameters( Activity activity , BrushParameters bp )
+    {
+        SharedPreferences prefs = activity.getSharedPreferences( SHARED_PREFS_NAME, Activity.MODE_PRIVATE);
+        Editor editor = prefs.edit();
+        editor.putInt( KEY_BRUSH_SIZE, bp.getBrushSize() );
+        editor.putInt( KEY_BRUSH_COLOR, bp.getColor() );
+        editor.putInt( KEY_BRUSH_COLOR_BASE, bp.getColorBase() );
+        editor.putInt( KEY_BRUSH_COLOR_INTENSITY, bp.getColorIntensity() );
+        editor.putBoolean(KEY_BLUR_EFFECT , bp.isBlur() );
+        editor.putBoolean(KEY_EMBOSS_EFFECT , bp.isEmboss() );
+        editor.commit();
+    }
+
+    public void restoreBrushParameters( Activity activity , BrushParameters bp )
+    {
+        SharedPreferences prefs = activity.getSharedPreferences(SHARED_PREFS_NAME, Activity.MODE_PRIVATE);
+        bp.setBrushSize( prefs.getInt( KEY_BRUSH_SIZE, DEFAULT_BRUSH_SIZE ));
+        bp.setColor( prefs.getInt( KEY_BRUSH_COLOR, DEFAULT_COLOR ));
+        bp.setColorBase( prefs.getInt( KEY_BRUSH_COLOR_BASE, DEFAULT_COLOR ));
+        bp.setColorIntensity( prefs.getInt( KEY_BRUSH_COLOR_INTENSITY, DEFAULT_INTENSITY ));
+        bp.setBrushSize( prefs.getInt( KEY_BRUSH_SIZE, DEFAULT_BRUSH_SIZE ));
+        bp.setBlur( prefs.getBoolean( KEY_BLUR_EFFECT , false ));
+        bp.setEmboss( prefs.getBoolean( KEY_EMBOSS_EFFECT , false ));
+    }
 
 }
