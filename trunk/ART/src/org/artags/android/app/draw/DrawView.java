d0f0fe79514bde3342afbdd006fe812453e8ea5e
pierre.levy@gmail.com <pierre.levy@gmail.com@c2dd0ccd-b6f9-2fe6-36c0-c3bc145f12f7>
2011-05-02 23:00:10 +0000

    Release 1.1 RC4 - thumbnail aspect ratio fixed

--- trunk/ART/src/org/artags/android/app/draw/DrawView.java
+++ trunk/ART/src/org/artags/android/app/draw/DrawView.java
@@ -38,11 +38,10 @@ import org.artags.android.app.apilevels.ApiLevels;
  */
 public class DrawView extends View
 {
+
     private static final int THUMBNAIL_SIZE = 200;
     private static final int THUMBNAIL_MARGIN = 40;
-
     private final int BACKGROUND_COLOR = 0xFFAAAAAA;
-
     private Paint mPaint;
     private Bitmap mBitmap;
     private Canvas mCanvas;
@@ -54,39 +53,33 @@ public class DrawView extends View
     private boolean mEyeDropper;
     private DrawActivity mActivity;
     private boolean haveMoved = false;//to draw only 1 point
-    private Point mGlobalOffset = new Point( 0 , 0 );
-
+    private Point mGlobalOffset = new Point(0, 0);
     //used in pinch
     private float oldDist;
     //private float oldAngle;
     private PointF mid;
     private PointF newMid;
-
     private Matrix matrix = null;
     private Matrix savedMatrix = null;
-
     private int touchMode;
     private final int NONE = 0;
     private final int ZOOM = 1;
     private final int DRAW = 2;
-            
-    public DrawView( DrawActivity activity, DisplayMetrics dm )
+
+    public DrawView(DrawActivity activity, DisplayMetrics dm)
     {
-        super( activity );
-        
-        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB )
+        super(activity);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
         {
             mGlobalOffset.y = 60; // ActionBar Height
         }
-        
+
         mActivity = activity;
 
         mHeight = dm.heightPixels;
         mWidth = dm.widthPixels;
-        this.
-
-
-        mBitmap = Bitmap.createBitmap( mWidth, mHeight, Bitmap.Config.ARGB_8888);
+        this.mBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
         mCanvas = new Canvas(mBitmap);
         mPath = new Path();
         mBitmapPaint = new Paint(Paint.DITHER_FLAG);
@@ -100,7 +93,7 @@ public class DrawView extends View
         super.onSizeChanged(w, h, oldw, oldh);
         mHeight = h;
         mWidth = w;
-        
+
 
     }
 
@@ -109,13 +102,13 @@ public class DrawView extends View
     {
         canvas.drawColor(BACKGROUND_COLOR);
         //draw the bitmap with the zoom
-        if(matrix == null || savedMatrix == null)
+        if (matrix == null || savedMatrix == null)
         {
             matrix = canvas.getMatrix();
             savedMatrix = canvas.getMatrix();
             matrix.reset();
         }
-       
+
         canvas.setMatrix(matrix);
         canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
         canvas.drawPath(mPath, mPaint);
@@ -137,7 +130,7 @@ public class DrawView extends View
     {
         float dx = Math.abs(x - mX);
         float dy = Math.abs(y - mY);
-        if (this.touchMode==DRAW && (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE))
+        if (this.touchMode == DRAW && (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE))
         {
             mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
             mX = x;
@@ -148,11 +141,13 @@ public class DrawView extends View
 
     private void touch_up()
     {
-        if (this.touchMode==DRAW)
+        if (this.touchMode == DRAW)
         {
             mPath.lineTo(mX, mY);
-            if(!haveMoved)
+            if (!haveMoved)
+            {
                 mCanvas.drawPoint(mX, mY, mPaint);
+            }
         }
         // commit the path to our offscreen
         mCanvas.drawPath(mPath, mPaint);
@@ -165,27 +160,28 @@ public class DrawView extends View
     public boolean onTouchEvent(MotionEvent event)
     {
         float x = getNewX(event.getX());
-        float y = getNewY(event.getY() + (float) mGlobalOffset.y );
+        float y = getNewY(event.getY() + (float) mGlobalOffset.y);
 
-        if( ! mEyeDropper )
+        if (!mEyeDropper)
         {
             switch (event.getAction())
             {
                 /*case MotionEvent.ACTION_POINTER_DOWN:
                 case MotionEvent.ACTION_POINTER_2_DOWN://deprecated but fail on my N1 without it.
-                    startZoom(event);
-                    break;*/
+                startZoom(event);
+                break;*/
                 case MotionEvent.ACTION_DOWN:
                     this.touchMode = DRAW;
                     touch_start(x, y);
                     invalidate();
                     break;
                 case MotionEvent.ACTION_MOVE:
-                    if(this.touchMode == ZOOM)
+                    if (this.touchMode == ZOOM)
                     {
                         zoom(event);
 
-                    } else {
+                    } else
+                    {
                         touch_move(x, y);
                         invalidate();
                     }
@@ -195,19 +191,18 @@ public class DrawView extends View
                     invalidate();
                     break;
                 default:
-                    if (ApiLevels.getApiLevel()>= 5 &&
-                            (event.getAction() == ApiLevel5.ACTION_POINTER_DOWN || event.getAction() == ApiLevel5.ACTION_POINTER_2_DOWN))
+                    if (ApiLevels.getApiLevel() >= 5
+                            && (event.getAction() == ApiLevel5.ACTION_POINTER_DOWN || event.getAction() == ApiLevel5.ACTION_POINTER_2_DOWN))
                     {
                         startZoom(event);
                     }
             }
-        }
-        else
+        } else
         {
-            if( event.getAction() == MotionEvent.ACTION_UP )
+            if (event.getAction() == MotionEvent.ACTION_UP)
             {
-                int color = mBitmap.getPixel( (int) x, (int) y );
-                mActivity.showBrushDialog( color );
+                int color = mBitmap.getPixel((int) x, (int) y);
+                mActivity.showBrushDialog(color);
                 mEyeDropper = false;
             }
 
@@ -220,7 +215,7 @@ public class DrawView extends View
         return mBitmap;
     }
 
-    public void setPaint( Paint paint )
+    public void setPaint(Paint paint)
     {
         mPaint = paint;
     }
@@ -228,22 +223,22 @@ public class DrawView extends View
     public void setBitmap(Bitmap bm)
     {
         mBitmap = bm.copy(Bitmap.Config.ARGB_8888, true);
-        Log.d( "ARTags" , " w " + mWidth + " h " + mHeight + " bmw " + bm.getWidth() + " bmh " + bm.getHeight() ); 
-        if( ( mHeight > mWidth ) && ( bm.getHeight() < bm.getWidth() ))
+        Log.d("ARTags", " w " + mWidth + " h " + mHeight + " bmw " + bm.getWidth() + " bmh " + bm.getHeight());
+        if ((mHeight > mWidth) && (bm.getHeight() < bm.getWidth()))
         {
             Matrix m = new Matrix();
             m.postRotate(90);
             mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(), mBitmap.getHeight(), m, true);
-            Log.d( "ARTAgs:Draw", "rotate +90");
+            Log.d("ARTAgs:Draw", "rotate +90");
         }
-        if( ( mHeight < mWidth ) && ( bm.getHeight() > bm.getWidth() ) )
+        if ((mHeight < mWidth) && (bm.getHeight() > bm.getWidth()))
         {
             Matrix m = new Matrix();
             m.postRotate(-90);
             mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(), mBitmap.getHeight(), m, true);
-            Log.d( "ARTAgs:Draw", "rotate +90");
+            Log.d("ARTAgs:Draw", "rotate +90");
         }
-        mCanvas = new Canvas( mBitmap );
+        mCanvas = new Canvas(mBitmap);
 
     }
 
@@ -254,10 +249,10 @@ public class DrawView extends View
 
     public void restore()
     {
-        if( mBitmapUndo != null )
+        if (mBitmapUndo != null)
         {
             mBitmap = Bitmap.createBitmap(mBitmapUndo);
-            mCanvas = new Canvas( mBitmap );
+            mCanvas = new Canvas(mBitmap);
             invalidate();
         }
     }
@@ -265,24 +260,26 @@ public class DrawView extends View
     public void reset()
     {
         save();
-        mBitmap = Bitmap.createBitmap( mWidth, mHeight, Bitmap.Config.ARGB_8888);
+        mBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
         mCanvas = new Canvas(mBitmap);
         invalidate();
     }
 
     public void setEyeDropperMode()
     {
-         mEyeDropper = true;
+        mEyeDropper = true;
 
     }
 
-
     public Bitmap getThumbnail()
     {
-        Bitmap thumbnail = Bitmap.createBitmap( THUMBNAIL_SIZE , THUMBNAIL_SIZE , Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas( thumbnail );
-        Rect rect = new Rect( THUMBNAIL_MARGIN , 0 , THUMBNAIL_SIZE - THUMBNAIL_MARGIN , THUMBNAIL_SIZE );
-        canvas.drawBitmap( mBitmap, null, rect , null);
+        Bitmap thumbnail = Bitmap.createBitmap(THUMBNAIL_SIZE, THUMBNAIL_SIZE, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(thumbnail);
+        Rect rect = (mBitmap.getWidth() < mBitmap.getHeight())
+                ? new Rect(THUMBNAIL_MARGIN, 0, THUMBNAIL_SIZE - THUMBNAIL_MARGIN, THUMBNAIL_SIZE)
+                : new Rect(0, THUMBNAIL_MARGIN, THUMBNAIL_SIZE, THUMBNAIL_SIZE - THUMBNAIL_MARGIN);
+
+        canvas.drawBitmap(mBitmap, null, rect, null);
         return thumbnail;
     }
 
@@ -290,7 +287,8 @@ public class DrawView extends View
     {
         oldDist = spacing(event);
         //oldAngle = angle(event);
-        if (oldDist > 50) {
+        if (oldDist > 50)
+        {
 
             savedMatrix.set(matrix);
             midPoint(mid, event);
@@ -303,7 +301,8 @@ public class DrawView extends View
     {
         float newDist = spacing(event);
         //float newAngle = angle(event);
-        if (newDist > 50) {
+        if (newDist > 50)
+        {
             matrix.set(savedMatrix);
             float scale = newDist / oldDist;
             //float angle = newAngle - oldAngle;
@@ -311,59 +310,66 @@ public class DrawView extends View
 
             //translate the picture if both fingers moved
             midPoint(newMid, event);
-            matrix.postTranslate(newMid.x-mid.x, newMid.y-mid.y);
+            matrix.postTranslate(newMid.x - mid.x, newMid.y - mid.y);
             //matrix.postRotate(angle);
 
             //be sure nothing outside the image will be visible
             float[] matrixValues = new float[9];
             matrix.getValues(matrixValues);
 
-            if(matrixValues[Matrix.MTRANS_X]>0)
+            if (matrixValues[Matrix.MTRANS_X] > 0)
+            {
                 matrixValues[Matrix.MTRANS_X] = 0;
+            }
 
-            if(matrixValues[Matrix.MTRANS_Y]>0)
+            if (matrixValues[Matrix.MTRANS_Y] > 0)
+            {
                 matrixValues[Matrix.MTRANS_Y] = 0;
+            }
 
-            if(matrixValues[Matrix.MSCALE_X]<1)
+            if (matrixValues[Matrix.MSCALE_X] < 1)
             {
                 matrixValues[Matrix.MSCALE_X] = 1;
                 matrixValues[Matrix.MSCALE_Y] = 1;
             }
 
             //let's do some math to know how many pixels are empty at the bottom and/or right of the screen
-            float bottomPadding = (mHeight * (1-matrixValues[Matrix.MSCALE_Y])) - matrixValues[Matrix.MTRANS_Y];
-            float rightPadding = (mWidth * (1-matrixValues[Matrix.MSCALE_X])) - matrixValues[Matrix.MTRANS_X];
-            
-            if(bottomPadding > 0)
+            float bottomPadding = (mHeight * (1 - matrixValues[Matrix.MSCALE_Y])) - matrixValues[Matrix.MTRANS_Y];
+            float rightPadding = (mWidth * (1 - matrixValues[Matrix.MSCALE_X])) - matrixValues[Matrix.MTRANS_X];
+
+            if (bottomPadding > 0)
             {
                 matrixValues[Matrix.MTRANS_Y] += bottomPadding;
             }
-            if(rightPadding > 0)
+            if (rightPadding > 0)
             {
                 matrixValues[Matrix.MTRANS_X] += rightPadding;
             }
 
             matrix.setValues(matrixValues);
 
-            
+
             invalidate();
         }
     }
 
-    private float spacing(MotionEvent event) {
+    private float spacing(MotionEvent event)
+    {
         float x = 0;
         float y = 0;
-        if (ApiLevels.getApiLevel()>= 5)
+        if (ApiLevels.getApiLevel() >= 5)
         {
             x = ApiLevel5.getX(event, 0) - ApiLevel5.getX(event, 1);
             y = ApiLevel5.getY(event, 0) - ApiLevel5.getY(event, 1);
         }
         return FloatMath.sqrt(x * x + y * y);
     }
-    private void midPoint(PointF point, MotionEvent event) {
+
+    private void midPoint(PointF point, MotionEvent event)
+    {
         float x = 0;
         float y = 0;
-        if (ApiLevels.getApiLevel()>= 5)
+        if (ApiLevels.getApiLevel() >= 5)
         {
             x = ApiLevel5.getX(event, 0) + ApiLevel5.getX(event, 1);
             y = ApiLevel5.getY(event, 0) + ApiLevel5.getY(event, 1);
@@ -371,19 +377,22 @@ public class DrawView extends View
         point.set(x / 2, y / 2);
     }
     /*private float angle(MotionEvent event) {
-        float x = event.getX(0) - event.getX(1);
-        float y = event.getY(0) - event.getY(1);
-        Double angle = Math.toDegrees(Math.atan2(y, x));
-        return angle.floatValue();
+    float x = event.getX(0) - event.getX(1);
+    float y = event.getY(0) - event.getY(1);
+    Double angle = Math.toDegrees(Math.atan2(y, x));
+    return angle.floatValue();
     }*/
 
-    private float getNewX(float x) {
+    private float getNewX(float x)
+    {
         float[] matrixValues = new float[9];
         matrix.getValues(matrixValues);
         float newX = (x - matrixValues[Matrix.MTRANS_X]) / matrixValues[Matrix.MSCALE_X];
         return newX;
     }
-    private float getNewY(float y) {
+
+    private float getNewY(float y)
+    {
         float[] matrixValues = new float[9];
         matrix.getValues(matrixValues);
         float newY = (y - matrixValues[Matrix.MTRANS_Y]) / matrixValues[Matrix.MSCALE_Y];
